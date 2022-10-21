b3670c7149899874372fb5bbef33233f5b5ac7ec
msaww <113406050+msaww@users.noreply.github.com>
2022-10-21 12:40:25 -0400

    Update helloworld_test.js

--- Truffle/test/helloworld_test.js
+++ Truffle/test/helloworld_test.js
@@ -1,14 +1,14 @@
-const HelloWorld = artifacts.require("HelloWorld");
+const Helloworld = artifacts.require("Helloworld");
 
-contract("HelloWorld", accounts => {
+contract("Helloworld", accounts => {
     it("constructor should set the message correctly", async () => {
-        let instance = await HelloWorld.deployed();
+        let instance = await Helloworld.deployed();
         let message = await instance.message();
         assert.equal(message, "Hello World constructor");
     });
 
     it("owner should be accounts[0]", async () => {
-        let instance = await HelloWorld.deployed();
+        let instance = await Helloworld.deployed();
         let owner = await instance.owner();
         assert.equal(owner, accounts[0]);
     });
